ff83670a2828cb0d4cceba4331f7100c791f320f
n-chr <131326994+n-chr@users.noreply.github.com>
2024-10-08 09:16:52 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// JavaScript
+
+console.log('Hello world!');
+
+const btn = document.querySelector('#btn');
+
+btn.addEventListener('click', () => {
+  document.body.classList.toggle('black');
+});
